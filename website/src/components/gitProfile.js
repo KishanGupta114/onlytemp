6073fb936c0e18e46e01ed9302727460cf418c23
kishan <kkishan114@gmail.com>
2020-03-20 14:25:51 +0530

    initial commit

---
+++ website/src/components/gitProfile.js
@@ -0,0 +1,6 @@
+import { Github } from 'react-social-github';
+import React from 'react';
+
+return default const GitProfile = (props) =>(
+    <Github type="widget" fab={false} fabCorner="" iconColor="" iconWidth="" iconHeight="" linkText="" tooltipOnHover={true} repo="react" user="facebook" ></Github>
+);
